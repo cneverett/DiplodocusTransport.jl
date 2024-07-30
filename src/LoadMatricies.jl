00d8c6891503c2af9baf337ecc591386a52af5c3
Christopher Everett <cn.everett@outlook.com>
2024-07-30 14:44:56 +0100

    + function to load interaction matrices

---
+++ src/LoadMatricies.jl
@@ -0,0 +1,70 @@
+function LoadMatricies(CollisionMatricies::Dict{String,Tuple},DataDirectory::String)
+
+    for i in 1:length(interaction_list)
+
+        interaction = interaction_list[i]
+
+        name1_loc = findfirst(==(interaction[1]),name_list)
+        name2_loc = findfirst(==(interaction[2]),name_list)
+        name3_loc = findfirst(==(interaction[3]),name_list)
+        name4_loc = findfirst(==(interaction[4]),name_list)
+
+        name1 = name_list[name1_loc]
+        name2 = name_list[name2_loc]
+        name3 = name_list[name3_loc]
+        name4 = name_list[name4_loc]
+
+        nump1 = string(nump_list[name1_loc])
+        nump2 = string(nump_list[name2_loc])
+        nump3 = string(nump_list[name3_loc])
+        nump4 = string(nump_list[name4_loc])
+        numt1 = string(numt_list[name1_loc])
+        numt2 = string(numt_list[name2_loc])
+        numt3 = string(numt_list[name3_loc])
+        numt4 = string(numt_list[name4_loc])
+
+        pl1 = string(pl_list[name1_loc])
+        pl2 = string(pl_list[name2_loc])
+        pl3 = string(pl_list[name3_loc])
+        pl4 = string(pl_list[name4_loc])
+
+        pu1 = string(pu_list[name1_loc])
+        pu2 = string(pu_list[name2_loc])
+        pu3 = string(pu_list[name3_loc])
+        pu4 = string(pu_list[name4_loc])
+
+        filename = name1*name2*name3*name4*"#"*pl1*"#"*pu1*"#"*nump1*"#"*pl2*"#"*pu2*"#"*nump2*"#"*pl3*"#"*pu3*"#"*nump3*"#"*pl4*"#"*pu4*"#"*nump4*"#"*numt1*"#"*numt2*"#"*numt3*"#"*numt4*".jld2"
+
+        matricies = fload_Matrix(DataDirectory,filename)
+
+        if interaction[1] == interaction[2] && interaction[3] == interaction[4]
+            SMatrix = SixDtoThreeD(matricies[1])
+            TMatrix = FourDtoTwoD(matricies[2])
+            CollisionMatricies[interaction] = (SMatrix,TMatrix)
+        end
+    
+        if interaction[1] == interaction[2] && interaction[3] != interaction[4]
+            SMatrix3 = SixDtoThreeD(matricies[1])
+            SMatrix4 = SixDtoThreeD(matricies[2])
+            TMatrix = FourDtoTwoD(matricies[3])
+            CollisionMatricies[interaction] = (SMatrix3,SMatrix4,TMatrix)
+        end
+    
+        if interaction[1] != interaction[2] && interaction[3] == interaction[4]
+            SMatrix = SixDtoThreeD(matricies[1])
+            TMatrix1 = FourDtoTwoD(matricies[2])
+            TMatrix2 = FourDtoTwoD(matricies[3])
+            CollisionMatricies[interaction] = (SMatrix,TMatrix1,TMatrix2)
+        end
+    
+        if interaction[1] != interaction[2] && interaction[3] != interaction[4]
+            SMatrix3 = SixDtoThreeD(matricies[1])
+            SMatrix4 = SixDtoThreeD(matricies[2])
+            TMatrix1 = FourDtoTwoD(matricies[3])
+            TMatrix2 = FourDtoTwoD(matricies[4])
+            CollisionMatricies[interaction] = (SMatrix3,SMatrix4,TMatrix1,TMatrix2)
+        end
+
+    end
+
+end
