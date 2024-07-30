aa67fc91c5068ee2b9b9547e71a55924ce58c595
Christopher Everett <cn.everett@outlook.com>
2024-07-30 14:45:59 +0100

    + moments functions

---
+++ src/DistributionMoments.jl
@@ -0,0 +1,67 @@
+"""
+    numberDensity(f0,dp,dμ)
+
+Returns the average number density of a distribution function `f`. Number density is defined as the zeroth moment of the distribution function. i.e. 
+    ```math
+    n = \\int \\mathrm{d}p\\mathrm{d}\\mu f(p,μ) = \\sum_{i,j} f_{ij} \\Delta p_i \\Delta μ_j
+    ``` 
+where `dp` = ``\\Delta p_i`` is a vector of momentum intervals and `dμ` = ``\\Delta μ_j`` is a vector of cosine (momentum space) angle intervals.
+"""
+function NumberDensity(f::Array{Float32},dp::Vector{Float32},dμ::Vector{Float32})
+
+    n = dp' * f * dμ
+    
+    return n
+    
+end
+
+"""
+    momentum(f0,dp,meanp,dμ)
+
+Returns the average momentum of a distribution function `f`. average momentum is defined as the first moment of the distribution function. i.e. 
+    ```math
+    \\braket{p} = \\frac{\\int \\mathrm{d}p\\mathrm{d}\\mu pf(p,μ)}{n} = \\sum_{i,j} f_{ij} \\Delta p_i\\braket{p}_i \\Delta μ_j / n
+    ``` 
+where `dp` = ``\\Delta p_i`` is a vector of momentum intervals, `meanp` = ``\\braket{p}_i`` is a vector of the average momentum value per bin, `dμ` = ``\\Delta μ_j`` is a vector of cosine (momentum space) angle intervals, and `numberDensity` = ``n`` is the average number density calculated using the function [`numberDensity`](@ref).
+"""
+function Momentum(f::Array{Float32},dp::Vector{Float32},meanp::Vector{Float32},dμ::Vector{Float32},numberDensity::Float32)
+
+    dpmeanp = dp .* meanp
+    momentum = dpmeanp' * f * dμ
+    momentum /= numberDensity
+
+    return momentum
+end
+
+
+"""
+    energy(f0,dp,meanp,dμ)
+
+Returns the average TOTAL energy of a distribution function `f`. average energy is defined as the first moment of the distribution function. i.e. 
+    ```math
+    \\braket{p} = \\frac{\\int \\mathrm{d}p\\mathrm{d}\\mu p^0f(p,μ)}{n} = \\sum_{i,j} f_{ij} \\Delta E_i \\Delta μ_j / n
+    ``` 
+where `ΔE` = ``\\Delta E_i`` is a vector of the average "energy" value per bin (has dimensions of momentum squared), `dμ` = ``\\Delta μ_j`` is a vector of cosine (momentum space) angle intervals, and `numberDensity` = ``n`` is the average number density calculated using the function [`numberDensity`](@ref).
+"""
+function Energy(f::Array{Float32},ΔE::Vector{Float32},dμ::Vector{Float32},numberDensity::Float32)
+
+    energy = ΔE' * f * dμ
+    energy /= numberDensity
+
+    return energy
+    
+end
+
+function temperature(f0::Array{Float32},ΔEkin::Vector{Float32},dμ::Vector{Float32},numberDensity::Float32)
+
+    Ekin = transpose(ΔEkin) * f0 * dμ
+    Ekin /= numberDensity
+
+    #  Ekin = 3/2 kB * T 
+    # Ekin = E - mass 
+    #println(Ekin)
+    Temperature = (Ekin) * 1836.1528f0 * 9.11e-31 * 3f8^2 / 1.38f-23 * (2/3)  # E = 3 kb T 
+
+    return Temperature
+    
+end
