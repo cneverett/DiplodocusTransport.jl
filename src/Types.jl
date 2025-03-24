6b35441b2a973cc17812edafde4b9d584fe08587
cneverett <cn.everett@outlook.com>
2025-03-24 16:06:04 +0100

    removed redundant type definitions

--- src/Types.jl
+++ src/Types.jl
@@ -7,26 +7,41 @@ abstract type ModeType end
 fType = Union{AbstractArray,ArrayPartition}
 
 # ModeType definitions
-abstract type IsoType <: ModeType end
-struct Iso <: IsoType end
-abstract type AxiType <: ModeType end
-struct Axi <: AxiType end
-abstract type AniType <: ModeType end
-struct Ani <: AniType end
+#abstract type IsoType <: ModeType end
+struct Iso <: ModeType end
+#abstract type AxiType <: ModeType end
+struct Axi <: ModeType end
+#abstract type AniType <: ModeType end
+struct Ani <: ModeType end
 
 # CoordinateType definitions
 abstract type SphericalType <: CoordinateType end
 struct Spherical <: SphericalType
-    mode::ModeType
 end
 
 abstract type CylindricalType <: CoordinateType end
 struct Cylindrical <: CylindricalType end
-
 
 # ForceType definitions
 abstract type CoordinateForceType <: ForceType end
 struct CoordinateForce <: CoordinateForceType end
 
 abstract type SyncRadReactType <: ForceType end
-struct SyncRadReact <: SyncRadReactType end
+struct SyncRadReact <: SyncRadReactType
+    mode::ModeType
+end
+
+# InteractionType definitions
+struct BinaryStruct
+    name1::String
+    name2::String
+    name3::String
+    name4::String
+end
+struct EmiStruct
+    name1::String
+    name2::String
+    name3::String
+    EmiName::String
+    mode::ModeType
+end
