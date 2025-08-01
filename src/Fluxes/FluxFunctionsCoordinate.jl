a4f22e5280eefdd7ccd7967bf7142699a4f5d740
Christopher Everett <cn.everett@outlook.com>
2025-08-01 15:41:17 +0100

    update flux scaling

--- src/Fluxes/FluxFunctionsCoordinate.jl
+++ src/Fluxes/FluxFunctionsCoordinate.jl
@@ -14,7 +14,7 @@
         flux *= (-p064 + p164) * (-u0 + u1) * (-phi0 + phi1)
 
         # hacky fix for time being dependent on grid size
-        flux *= (p164 + p064) / 2 / (p164 - p064) # mp/dp
+        flux *= (p164 + p064) / 2 / (p164 - p064) * 2pi # mp/dp
 
         return flux
 
