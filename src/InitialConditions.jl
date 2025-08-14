c5fe77c5c8668b8b72764d466dbee8bfcd818f87
Christopher Everett <cn.everett@outlook.com>
2025-08-14 16:35:44 +0100

    + added state vector

--- src/InitialConditions.jl
+++ src/InitialConditions.jl
@@ -1,4 +1,63 @@
 """
+    Initialise_Initial_Condition(PhaseSpace)
+
+Returns a zero vector with elements for the initial conditions of all particles at all positions in phase space. 
+"""
+function Initialise_Initial_Condition(PhaseSpace::PhaseSpaceStruct)
+
+    px_num_list = PhaseSpace.Momentum.px_num_list
+    py_num_list = PhaseSpace.Momentum.py_num_list
+    pz_num_list = PhaseSpace.Momentum.pz_num_list
+    x_num = PhaseSpace.Space.x_num
+    y_num = PhaseSpace.Space.y_num
+    z_num = PhaseSpace.Space.z_num
+    
+    n = sum(px_num_list.*py_num_list.*pz_num_list)
+    m = n*x_num*y_num*z_num
+
+    return zeros(Float32,m)
+
+end
+
+function Location_Species_To_StateVector(Initial::Vector{Float32},PhaseSpace::PhaseSpaceStruct;x_idx::Int64=1,y_idx::Int64=1,z_idx::Int64=1,species_index::Int64=0)
+
+    if iszero(species_index)
+        error("Species not defined")
+    end
+    x_num = PhaseSpace.Space.x_num
+    y_num = PhaseSpace.Space.y_num
+    z_num = PhaseSpace.Space.z_num
+
+    px_num_list = PhaseSpace.Momentum.px_num_list
+    py_num_list = PhaseSpace.Momentum.py_num_list
+    pz_num_list = PhaseSpace.Momentum.pz_num_list
+
+    px_num = px_num_list[species_index]
+    py_num = py_num_list[species_index]
+    pz_num = pz_num_list[species_index]
+
+    name_list = PhaseSpace.name_list
+
+    off_space = off_space = (x_idx-1)*y_num*z_num+(y_idx-1)*z_num+z_idx-1
+    offset = zeros(Int64,size(name_list)[1])
+    for i in eachindex(offset)
+        if i == 1
+            offset[i] = 0
+        else
+            offset[i] = offset[i-1]+px_num_list[i-1]*py_num_list[i-1]*pz_num_list[i-1]
+        end
+    end
+    off_name = offset[species_index]
+
+    start_idx = 1+off_name+off_space
+    end_idx = pz_num*px_num*py_num+off_name+off_space
+
+    return @view(Initial[start_idx:end_idx])
+
+end
+
+
+"""
     Initial_PowerLaw(Lists,species,pmin,pmax,umin,uman,hmin,hmax,index,num_Init)
 
 A power-law distribution is typically defined by N(E) ∝ E^(-index). N(E) = f(E) therefore f(p) for a power-law distribution is given by f(p) = f(E)*dE/dp = E^(-index) * p/E = pE^(-index-1). Averaging this over a cell gives f(p)_avg = [E^(1-index)/(1-index)]/[p] where [] denote evaluation at the cell bounds.
@@ -82,6 +141,91 @@ function Initial_PowerLaw(PhaseSpace::PhaseSpaceStruct,species::String,pmin::S,p
     f0_species = reshape(f0_3D_species,p_num*u_num*h_num)
 
     return Float32.(f0_species)
+end
+
+function Initial_PowerLaw!(Initial::Vector{Float32},PhaseSpace::PhaseSpaceStruct,species::String,pmin::S,pmax::S,umin::S,umax::S,hmin::S,hmax::S,index::AbstractFloat,num_Init::AbstractFloat) where S <: Union{Float32,Float64,Int64}
+
+    Momentum = PhaseSpace.Momentum
+
+    name_list = PhaseSpace.name_list
+    p_up_list = Momentum.px_up_list
+    p_low_list = Momentum.px_low_list
+    p_grid_list = Momentum.px_grid_list
+    p_num_list = Momentum.px_num_list
+    u_grid_list = Momentum.py_grid_list
+    u_num_list = Momentum.py_num_list
+    h_num_list = Momentum.pz_num_list
+    h_grid_list = Momentum.pz_grid_list
+
+    Grids = PhaseSpace.Grids
+    pr_list = Grids.pxr_list
+    mp_list = Grids.mpx_list
+    mu_list = Grids.mpy_list
+    dp_list = Grids.dpx_list
+    du_list = Grids.dpy_list
+
+    species_index = findfirst(==(species),name_list)
+    pr = pr_list[species_index]
+    dp = dp_list[species_index]
+    du = du_list[species_index]
+
+    f0_3D_species = zeros(Float64,p_num_list[species_index],u_num_list[species_index],h_num_list[species_index])
+
+    # Set initial conditions goes here
+    pu = Float64(p_up_list[species_index])
+    pl = Float64(p_low_list[species_index])
+    p_num = p_num_list[species_index]
+    p_grid = p_grid_list[species_index]
+    u_num = u_num_list[species_index]
+    u_grid = u_grid_list[species_index]
+    h_num = h_num_list[species_index]
+    h_grid = h_grid_list[species_index]
+    #pr = DC.bounds(pl,pu,p_num,p_grid)
+    #dp = DC.deltaVector(pr)
+    #du = DC.deltaVector(DC.bounds(DC.u_low,DC.u_up,u_num,u_grid))
+    mass = getfield(DC,Symbol("mu"*name_list[species_index]))
+
+    type = zero(S)
+    if (typeof(type) == Float32) || (typeof(type) == Float64) 
+        pmin_index = DC.location(pl,pu,p_num,Float64(pmin),p_grid)
+        pmax_index = DC.location(pl,pu,p_num,Float64(pmax),p_grid)
+        umin_index = DC.location(DC.u_low,DC.u_up,u_num,Float64(umin),u_grid)
+        umax_index = DC.location(DC.u_low,DC.u_up,u_num,Float64(umax),u_grid)
+        hmin_index = DC.location(DC.h_low,DC.h_up,h_num,Float64(hmin),h_grid)
+        hmax_index = DC.location(DC.h_low,DC.h_up,h_num,Float64(hmax),h_grid)
+    elseif typeof(type)==Int64
+        pmin_index = pmin
+        pmax_index = pmax
+        umin_index = umin
+        umax_index = umax
+        hmin_index = hmin
+        hmax_index = hmax
+    end
+    
+    #println(pmin_index,pmax_index,umin_index,umax_index)
+
+    # power law averaged over cell width.
+    for px in pmin_index:pmax_index, py in umin_index:umax_index, pz in hmin_index:hmax_index 
+        f0_3D_species[px,py,pz] = sqrt(mass^2+pr[px+1]^2)^(1-index) - sqrt(mass^2+pr[px]^2)^(1-index)
+        f0_3D_species[px,py,pz] /= 1-index
+        #f0_3D_species[px,py,pz] /= (pr[px+1]-pr[px])
+    end
+    # set values and normlaise to initial number density (in m^{-3})
+    num = sum(f0_3D_species)
+    f0_3D_species .*= num_Init/num
+
+    # scale by dp*du 
+    #=for i in axes(u0_2D_species,1), j in axes(u0_2D_species,2)
+        u0_2D_species[i,j] *= dp[i] * du[j]
+    end=#
+
+    f0_species = reshape(f0_3D_species,p_num*u_num*h_num)
+
+    Initial_local = Location_Species_To_StateVector(Initial,PhaseSpace,species_index=species_index)
+
+    Initial_local .+= f0_species
+
+    return nothing
 end
 
 """
