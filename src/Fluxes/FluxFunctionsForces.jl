a5c99585f67a1983db7feb184cd5ff0b71095184
Christopher Everett <cn.everett@outlook.com>
2025-09-10 17:40:35 +0100

    + spherical rad reaction force

--- src/Fluxes/FluxFunctionsForces.jl
+++ src/Fluxes/FluxFunctionsForces.jl
@@ -1,4 +1,4 @@
-# =============== IJK Sync Rad Reaction ================== #
+# ======= IJK Sync Rad Reaction Cylindrical ============== #
 
     function IFluxFunction(force::SyncRadReact,space_coords::Cylindrical,momentum_coords::Spherical,t0,t1,x0,x1,y0,y1,z0,z1,p,u0,u1,phi0,phi1,name::String)
 
@@ -17,26 +17,25 @@
         mEle = getfield(DC,Symbol("mEle"))
         c = getfield(DC,Symbol("c"))
 
-        fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed 
+        fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed
+        
+        flux::Float64 = fluxScale
 
         if m == 0 || Z == 0
-            return flux = 0f0
+            flux *= 0e0
         else
             if typeof(mode) == Ani
-                flux = 1/(6*m^2)
+                flux *= 1/(6*m^2)
                 flux *= p64*sqrt(m^2 + p64^2) * (-3*u0 + u0^3 + 3*u1 - u1^3) * (phi0 - phi1)
                 flux *= (t0 - t1) * (x0^2 - x1^2) * (y0 - y1) * (z0 - z1)
-                flux *= fluxScale
             elseif typeof(mode) == Axi # average over azimuthal angles
-                flux = 1/(6*m^2)
+                flux *= 1/(6*m^2)
                 flux *= p64*sqrt(m^2 + p64^2) * (-3*u0 + u0^3 + 3*u1 - u1^3) * (phi0 - phi1)
                 flux *= (t0 - t1) * (x0^2 - x1^2) * (y0 - y1) * (z0 - z1)
-                flux *= fluxScale
             elseif typeof(mode) == Iso # averaged force over all angles
-                flux = -1/(3*m^2)
+                flux *= -1/(3*m^2)
                 flux *= p64*sqrt(m^2 + p64^2) * (phi0 - phi1) * (u0 - u1)
                 flux *= (t0 - t1) * (x0^2 - x1^2) * (y0 - y1) * (z0 - z1)
-                flux *= fluxScale
             else
                 error("Synchrotron mode not recognised.")
             end
@@ -66,21 +65,21 @@
 
         fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed
 
+        flux::Float64 = fluxScale
+
         if m == 0 || Z == 0
-            flux = 0f0
+            flux *= 0e0
         else
             if typeof(mode) == Ani
-                flux = 1/4
-                flux *= u * (-1 + u^2) * (phi0 - phi1) * log(((-p064 + sqrt(m^2 + p064^2)) * (p164 + sqrt(m^2 + p164^2))^2) / (m^2 * (p064 + sqrt(m^2 + p064^2))))
+                flux *= 1/4
+                flux *= u * (-1 + u^2) * (phi0 - phi1) * (-2asinh(p064/m)+2asinh(p164/m))
                 flux *= (t0 - t1) * (x0^2 - x1^2) * (y0 - y1) * (z0 - z1)
-                flux *= fluxScale
             elseif typeof(mode) == Axi
-                flux = 1/4
-                flux *= u * (-1 + u^2) * (phi0 - phi1) * log(((-p064 + sqrt(m^2 + p064^2)) * (p164 + sqrt(m^2 + p164^2))^2) / (m^2 * (p064 + sqrt(m^2 + p064^2))))
+                flux *= 1/4
+                flux *= u * (-1 + u^2) * (phi0 - phi1) * (-2asinh(p064/m)+2asinh(p164/m))
                 flux *= (t0 - t1) * (x0^2 - x1^2) * (y0 - y1) * (z0 - z1)
-                flux *= fluxScale
             elseif typeof(mode) == Iso
-                flux = 0f0
+                flux *= 0e0
             end
         end
 
@@ -108,11 +107,133 @@
 
         fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed
 
+        flux::Float64 = fluxScale
+
         if m == 0 || Z == 0
-            flux = 0f0
+            flux *= 0e0
         else
-            flux = 0f0
-            flux *= fluxScale
+            flux *= 0e0
+        end
+
+        return flux
+
+    end
+
+# ======================================================== #
+
+# ======= IJK Sync Rad Reaction Spherical ============== #
+
+    function IFluxFunction(force::SyncRadReact,space_coords::Spherical,momentum_coords::Spherical,t0,t1,x0,x1,y0,y1,z0,z1,p,u0,u1,phi0,phi1,name::String)
+
+        # Evaluate the flux through the I surface i.e. surface of constant p
+
+        mode = force.mode
+        B = force.B
+
+        # convert p to Float64 to ensure no floating point issues
+        p64 = Float64(p)
+
+        μ0 = getfield(DC,Symbol("μ0"))
+        m = getfield(DC,Symbol("mu"*name))
+        q = getfield(DC,Symbol("q"))
+        Z = getfield(DC,Symbol("z"*name))
+        mEle = getfield(DC,Symbol("mEle"))
+        c = getfield(DC,Symbol("c"))
+
+        fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed 
+
+        if m == 0 || Z == 0
+            return flux = 0f0
+        else
+            if typeof(mode) == Ani
+                flux *= -1/(9*m^2)
+                flux *= p64*sqrt(m^2 + p64^2) * (-3*u0 + u0^3 + 3*u1 - u1^3) * (phi0 - phi1)
+                flux *= (t0 - t1) * (x0^3 - x1^3) * (cospi(y0/pi) - cospi(y1/pi)) * (z0 - z1)
+            elseif typeof(mode) == Axi # average over azimuthal angles
+                flux *= -1/(9*m^2)
+                flux *= p64*sqrt(m^2 + p64^2) * (-3*u0 + u0^3 + 3*u1 - u1^3) * (phi0 - phi1)
+                flux *= (t0 - t1) * (x0^3 - x1^3) * (cospi(y0/pi) - cospi(y1/pi)) * (z0 - z1)
+            elseif typeof(mode) == Iso # averaged force over all angles
+                flux *= 2/(9*m^2)
+                flux *= p64*sqrt(m^2 + p64^2) * (phi0 - phi1) * (u0 - u1)
+                flux *= (t0 - t1) * (x0^3 - x1^3) * (cospi(y0/pi) - cospi(y1/pi)) * (z0 - z1)
+            else
+                error("Synchrotron mode not recognised.")
+            end
+        end
+
+        return flux
+
+    end
+
+    function JFluxFunction(force::SyncRadReact,space_coords::Spherical,momentum_coords::Spherical,t0,t1,x0,x1,y0,y1,z0,z1,p0,p1,u,phi0,phi1,name::String)
+
+        # Evaluate the flux through the J surface i.e. surface of constant u
+
+        mode = force.mode
+        B = force.B
+
+        # convert p to Float64 to ensure no floating point issues
+        p064 = Float64(p0)
+        p164 = Float64(p1)
+
+        μ0 = getfield(DC,Symbol("μ0"))
+        m = getfield(DC,Symbol("mu"*name))
+        q = getfield(DC,Symbol("q"))
+        Z = getfield(DC,Symbol("z"*name))
+        mEle = getfield(DC,Symbol("mEle"))
+        c = getfield(DC,Symbol("c"))
+
+        fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed
+
+        flux::Float64 = fluxScale
+
+        if m == 0 || Z == 0
+            flux *= 0e0
+        else
+            if typeof(mode) == Ani
+                flux *= -1/6
+                flux *= u * (-1 + u^2) * (phi0 - phi1) * (-2asinh(p064/m)+2asinh(p164/m))
+                flux *= (t0 - t1) * (x0^3 - x1^3) * (cospi(y0/pi) - cospi(y1/pi)) * (z0 - z1)
+            elseif typeof(mode) == Axi
+                flux *= -1/6
+                flux *= u * (-1 + u^2) * (phi0 - phi1) * (-2asinh(p064/m)+2asinh(p164/m))
+                flux *= (t0 - t1) * (x0^3 - x1^3) * (cospi(y0/pi) - cospi(y1/pi)) * (z0 - z1)
+            elseif typeof(mode) == Iso
+                flux *= 0e0
+            end
+        end
+
+        return flux
+
+    end
+
+    function KFluxFunction(force::SyncRadReact,space_coords::Spherical,momentum_coords::Spherical,t0,t1,x0,x1,y0,y1,z0,z1,p0,p1,u0,u1,phi,name::String)
+
+        # Evaluate the flux through the K surface i.e. surface of constant phi
+
+        mode = force.mode
+        B = force.B
+
+        # convert p to Float64 to ensure no floating point issues
+        p064 = Float64(p0)
+        p164 = Float64(p1)
+
+        μ0 = getfield(DC,Symbol("μ0"))
+        m = getfield(DC,Symbol("mu"*name))
+        q = getfield(DC,Symbol("q"))
+        Z = getfield(DC,Symbol("z"*name))
+        mEle = getfield(DC,Symbol("mEle"))
+        c = getfield(DC,Symbol("c"))
+
+        fluxScale = (Z^4*B^2)/(μ0*m^2*mEle*c^2) / (24) # normalised by σT*c, factor of 24 is to make sure timescale and energy conservation is correct, no idea why this is needed
+
+        flux::Float64 = fluxScale
+
+        if m == 0 || Z == 0
+            flux *= 0e0
+        else
+            flux *= 0e0
         end
 
         return flux
