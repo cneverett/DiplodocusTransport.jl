91401375adc2d7131bb1e54cee0254115817f05d
cnever <cn.everett@outlook.com>
2026-01-11 15:33:34 +0000

    + Memory optimisation for if Ele and Pos populations are identical

--- src/Collisions/LoadMatrices.jl
+++ src/Collisions/LoadMatrices.jl
@@ -91,7 +91,7 @@ function GainCorrection!(Output::Tuple{Tuple{String, String, String, String, Flo
 
 end
 
-function LoadMatrices_Binary(M_Bin::AbstractMatrix{F},DataDirectory::String,PhaseSpace::PhaseSpaceStruct;mode::ModeType=Ani(),corrected::Bool=true) where F<:AbstractFloat
+function LoadMatrices_Binary(M_Bin::AbstractMatrix{F},DataDirectory::String,PhaseSpace::PhaseSpaceStruct,mode::ModeType=Ani(),corrected::Bool=true,Bin_sparse::Bool=false) where F<:AbstractFloat
 
     Binary_list = PhaseSpace.Binary_list
 
@@ -128,17 +128,35 @@ function LoadMatrices_Binary(M_Bin::AbstractMatrix{F},DataDirectory::String,Phas
         name3 = interaction.name3
         name4 = interaction.name4
 
-        name1_loc = findfirst(==(name1),name_list)
-        name2_loc = findfirst(==(name2),name_list)
-        name3_loc = findfirst(==(name3),name_list)
-        name4_loc = findfirst(==(name4),name_list)
+        # Memory optimisation by allowing Ele and Pos populations to be modelled as identical thus only requiring one to be defined
+        if !isnothing(findfirst(==("Pos"),[name1,name2,name3,name4])) && isnothing(findfirst(==("Pho"),name_list)) # if "Pos" is in interactions but not in "name_list" ∴ "Ele" population is taken to be "Ele"+"Pos" with identical populations of each particle
+            if (name1,name2,name3,name4) == ("Pos","Pho","Pos","Pho") # "Pos" compton scattering
+                return # skip loading pos compton matrices as this is correctly accounted by ele population including pos population
+            elseif (name1,name2,name3,name4) == ("Ele","Pos","Pho","Pho") # "Ele" "Pos" annihilation
+                GainScale = 1/4.0 # ele and pos populations are half the total ele population so scale gain matrix by 1/4
+                name1_loc = findfirst(==(name1),name_list)
+                name2_loc = findfirst(==("Ele"),name_list)
+                name3_loc = findfirst(==(name3),name_list)
+                name4_loc = findfirst(==(name4),name_list)
+            elseif (name1,name2,name3,name4) == ("Pho","Pho","Ele","Pos") # "Ele" "Pos" pair production
+                GainScale = 1.0
+                name1_loc = findfirst(==(name1),name_list)
+                name2_loc = findfirst(==(name2),name_list)
+                name3_loc = findfirst(==(name3),name_list)
+                name4_loc = findfirst(==("Ele"),name_list)
+            end
+        else
+            GainScale = 1.0
+            name1_loc = findfirst(==(name1),name_list)
+            name2_loc = findfirst(==(name2),name_list)
+            name3_loc = findfirst(==(name3),name_list)
+            name4_loc = findfirst(==(name4),name_list)
+        end
 
-        # ele pos swap for compton
-        if name1 == "Pos" && name2 == "Pho" && name3 == "Pos" && name4 == "Pho"
+        # ele pos swap for compton i.e. use ele compton matrices for pos compton
+        if (name1,name2,name3,name4) == ("Pos","Pho","Pos","Pho")
             name1 = "Ele"
-            name2 = "Pho"
             name3 = "Ele"
-            name4 = "Pho"
         end
 
         px1_grid::String = px_grid_list[name1_loc]
@@ -184,26 +202,21 @@ function LoadMatrices_Binary(M_Bin::AbstractMatrix{F},DataDirectory::String,Phas
 
         Parameters = (name1,name2,name3,name4,m1,m2,m3,m4,px1_low,px1_up,px1_grid,px1_num,py1_grid,py1_num,pz1_grid,pz1_num,px2_low,px2_up,px2_grid,px2_num,py2_grid,py2_num,pz2_grid,pz2_num,px3_low,px3_up,px3_grid,px3_num,py3_grid,py3_num,pz3_grid,pz3_num,px4_low,px4_up,px4_grid,px4_num,py4_grid,py4_num,pz4_grid,pz4_num)
 
-        filename::String = DC.BinaryFileName(Parameters)
-
-        #Parameters=(name1,name2,name3,name4,m1,m2,m3,m4,px1_low,px1_up,px1_grid,px1_num,py1_grid,py1_num,pz1_grid,pz1_num,px2_low,px2_up,px2_grid,px2_num,py2_grid,py2_num,pz2_grid,pz2_num,px3_low,px3_up,px3_grid,px3_num,py3_grid,py3_num,pz3_grid,pz3_num,px4_low,px4_up,px4_grid,px4_num,py4_grid,py4_num,pz4_grid,pz4_num)
-        #filename = DC.FileName(Parameters)
-
-        #Parameters = (name1,name2,name3,name4,mu1,mu2,mu3,mu4,p1_low,p1_up,p1_grid,p1_num,u1_grid,u1_num,p2_low,p2_up,p2_grid,p2_num,u2_grid,u2_num,p3_low,p3_up,p3_grid,p3_num,u3_grid,u3_num,p4_low,p4_up,p4_grid,p4_num,u4_grid,u4_num)
-
-        #filename = "CompTestWeighted22.jld2"
+        filename::String = BinaryFileName(Parameters)
 
         println(filename)
 
-        Output = DC.BinaryFileLoad_Matrix(DataDirectory,filename,corrected=corrected)
+        Output = BinaryFileLoad_Matrix(DataDirectory,filename,corrected=corrected)
         Parameters = Output[1]
-        GainMatrix3 = Output[2]
-        GainMatrix4 = Output[3]
+        GainMatrix3 = Output[2] .* GainScale
+        GainMatrix4 = Output[3] .* GainScale
         LossMatrix1 = Output[4]
         LossMatrix2 = Output[5]
 
-        DC.DoesConserve(Output) # print conversion statistic
-        Fill_M_Bin!(M_Bin,interaction,PhaseSpace,GainMatrix3,GainMatrix4,LossMatrix1,LossMatrix2,mode)
+        name_locs = (name1_loc,name2_loc,name3_loc,name4_loc)
+
+        DoesConserve(Output) # print conversion statistic
+        Fill_M_Bin!(M_Bin,name_locs,PhaseSpace,GainMatrix3,GainMatrix4,LossMatrix1,LossMatrix2,mode=mode,Bin_sparse=Bin_sparse)
 
     end # for
 
