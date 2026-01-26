a17590e826daa7c455816687da42bdb651f07adc
Christopher N. Everett <cn.everett@outlook.com>
2026-01-26 20:49:43 +0000

    fix of ExB sign

--- src/DiplodocusTransport.jl
+++ src/DiplodocusTransport.jl
@@ -4,7 +4,7 @@ module DiplodocusTransport
     export PhaseSpaceStruct, MomentumStruct, SpaceStruct, TimeStruct, OutputStruct
     export BinaryStruct, EmiStruct, ForceType
     export Cylindrical, Spherical, Cartesian, Ani, Axi, Iso
-    export CoordinateForce, SyncRadReact
+    export CoordinateForce, SyncRadReact, ExB
     export BuildBigMatrices, BuildFluxMatrices
     export Initialise_Initial_Condition, Location_Species_To_StateVector, Initial_Constant, Initial_MaxwellJuttner, Initial_PowerLaw, Initial_Constant!, Initial_MaxwellJuttner!, Initial_PowerLaw!, Initial_BoostedPowerLaw!, Initial_BlackBody!
     export Solve, EulerStruct
