b590d97d71596a03adc6a70ac0da242f4dd35c0d
Christopher N. Everett <cn.everett@outlook.com>
2026-04-24 18:05:46 +0100

    updated coordinate transform

--- src/Spacetime/CoordinateTransforms.jl
+++ src/Spacetime/CoordinateTransforms.jl
@@ -1,36 +1,46 @@
 """
-    CoordinateTransform(pos,from::AbstractCoordinates,to::AbstractCoordinates)
+    CoordinateTransform(pos_from,pos_to,from::AbstractCoordinates,to::AbstractCoordinates)
 
-Returns the coordinates ``pos=[t,x,y,z]`` in the ``to`` coordinate system given the coordinates in the ``from`` coordinate system.
+Converts the coordinates `pos_from` in the ``from`` coordinate system to the ``to`` coordinate system, storing the result in `pos_to`.
 """
-CoordinateTransform(pos,from::AbstractCoordinates,to::AbstractCoordinates) = error("Coordinate transform function not defined for coordinates $(typeof(from)) to coordinates $(typeof(to)).")
+CoordinateTransform!(pos,from::AbstractCoordinates,to::AbstractCoordinates) = error("Coordinate transform function not defined for coordinates $(typeof(from)) to coordinates $(typeof(to)).")
 
 
-function CoordinateTransform(pos::AbstractVector{T},::Paraboloidal,::Cartesian) where T
+function CoordinateTransform(pos_from::AbstractVector{T},pos_to::AbstractVector{T},::Paraboloidal,::Cartesian) where T
 
-    t = pos[1]
-    ϕ = pos[2]
-    u = pos[3]
-    v = pos[4]
+    t = pos_from[1]
+    ϕ = pos_from[2]
+    u = pos_from[3]
+    v = pos_from[4]
 
     x::T = u*v*cos(ϕ)
     y::T = u*v*sin(ϕ)
     z::T = 1/2 * (u^2 - v^2)
 
-    return SVector{4,T}(t,x,y,z)
+    pos_to[1] = t 
+    pos_to[2] = x
+    pos_to[3] = y
+    pos_to[4] = z
+
+    return nothing
 
 end
 
-function CoordinateTransform(pos::AbstractVector{T},::Paraboloidal,::Cylindrical) where T
+function CoordinateTransform(pos_from::AbstractVector{T},pos_to::AbstractVector{T},::Paraboloidal,::Cylindrical) where T
 
-    t = pos[1]
-    ϕ = pos[2]
-    u = pos[3]
-    v = pos[4]
+    t = pos_from[1]
+    ϕ = pos_from[2]
+    u = pos_from[3]
+    v = pos_from[4]
 
     ρ::T = u*v
     z::T = 1/2 * (u^2 - v^2)
 
-    return SVector{4,T}(t,ρ,ϕ,z)
+    pos_to[1] = t
+    pos_to[2] = ρ
+    pos_to[3] = ϕ
+    pos_to[4] = z
+
+    return nothing
 
 end
